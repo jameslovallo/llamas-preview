4f8a5217fd8dc013971d45ddec8755633622d35b
James Lovallo <patrickonparker@gmail.com>
2021-12-16 21:05:36 -0500

    fixes for astro upgrade

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,6 @@ export default function Preview() {
 							name="viewport"
 							content="initial-scale=1.0, width=device-width"
 						/>
-						<script type="module" src="https://spicy-sections.glitch.me/SpicySections.js"></script>
 					</Head>
 					<AppDrawer global={global} />
 					<AppBar global={global} />
@@ -74,11 +73,16 @@ export default function Preview() {
 					{page.length > 0 && (
 						<>
 							<Head>
-								{['drawer', 'dialog', 'form', 'images', 'video'].map(
+								<script
+									type="module"
+									src="https://unpkg.com/tua-body-scroll-lock@1.2.1/dist/tua-bsl.umd.js"
+								></script>
+								{['drawer', 'dialog', 'form', 'images', 'tabs', 'video'].map(
 									(script, index) => (
 										<script
-											src={`https://thevillagesofdetroit.com/_astro/src/js/${script}.js`}
+											src={`https://raw.githack.com/kaina-agency/llamas/master/public/js/${script}.js`}
 											key={index}
+											type="module"
 										/>
 									)
 								)}
